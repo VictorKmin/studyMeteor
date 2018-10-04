0a28df0f67067ae8f597d7957ef46e120b824668
Victor Kmin <kminvictor@gmail.com>
2018-10-04 18:00:59 +0300

    Upgrade CSS and HTML. Almost done "Upgrade" button

--- layouts/servicesPage/servicesLayout.js
+++ layouts/servicesPage/servicesLayout.js
@@ -16,33 +16,49 @@ try {
         },
 
 
-        'click #description'(event) {
+        'click #update'() {
+            // 'click #description'(event) {
             const id = this._id;
-            const title = $(event.target);
-            const content = title.text();
-            const parent = title.parents()[0];
-            title.remove();
+            //Місце, на яке нажали
+            // const title = event.target
+
+            // Шукаємо div idшкою description (аналог document.getElementById)
+            let some = event.path[1];
+            console.log(some);
+            const desc = $('#description');
+            //Текст всередині кнопочки
+            const content = desc.text();
+            // Дівка, де знаходиться наша кнопочка
+            const parent = desc.parents()[0];
+            // Фіча. шо  не плодити куча кнопочок
+            desc.remove();
+            //Створюю нове поле з інпутом
             $(parent).prepend($('<input type="text"/>')
-                .val(content)
-                //Тут навішую колбек на івент.
-                .on('blur', event => onDescriptionInputBlur(id, event.target))
-                .addClass('editDescription')
+                // В яке кладу значення
+                    .val(content)
+                    //Тут навішую колбек на івент brur - віджаття кнопки
+                    .on('blur', event => onDescriptionInputBlur(id, event.target))
+                    // і даю для нього клас, що б можна було знайти потім той клас
+                    .addClass('editDescription')
             );
+            // фокусуємся на клас. якйи ми створили вище
             $('.editDescription').focus();
-        }
+        },
     });
+
+
     const onDescriptionInputBlur = (id, el) => {
-        const value = el.value;
+        const value = el.value.trim();
         const parent = $(el).parents()[0];
         $(parent).prepend($('<div id="description" />').text(value));
-        Meteor.call("updateService", {id,description: value});
+        Meteor.call("updateService", {id, description: value});
         $(el).remove();
     };
 
     Template.services.helpers({
         findAllServices() {
             return Services.find({})
-        }
+        },
     });
 } catch (e) {
     document.write(e)
