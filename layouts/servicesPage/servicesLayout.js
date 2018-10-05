ee196443f5ea351d2866aa042928bcdd42fcb8ae
Victor Kmin <kminvictor@gmail.com>
2018-10-05 16:12:14 +0300

    Fix "Update" button. Add comments to code

--- layouts/servicesPage/servicesLayout.js
+++ layouts/servicesPage/servicesLayout.js
@@ -3,6 +3,12 @@ import {Services} from "../../collection";
 import {Meteor} from "meteor/meteor";
 
 try {
+
+    /**
+     * Create service with params from front-end page /service
+     * @param title - input text from front end
+     * @param description - input text from front end
+     */
     Template.services.events({
         'click #create'() {
             const title = document.getElementById('newServiceTitle').value;
@@ -10,32 +16,34 @@ try {
             Meteor.call("insertService", {title, description});
         },
 
+        /**
+         * Method delete view from DataBase
+         * @param this._id - id of div in DataBase
+         */
         'click #delete'() {
             console.log(`delete ${this._id}`);
             Services.remove({_id: this._id});
         },
 
+        /**
+         * This method update description text when button "update" was clicked
+         * To update view we need to call onDescriptionInputBlur method with id of view and event target
+         */
 
         'click #update'() {
-            // 'click #description'(event) {
             const id = this._id;
-            //Місце, на яке нажали
-            // const title = event.target
-
-            // Шукаємо div idшкою description (аналог document.getElementById)
-            let some = event.path[1];
-            console.log(some);
-            const desc = $('#description');
+            // jQuery Magic
+            const desc = $(event.path[1]).find('#description');
             //Текст всередині кнопочки
-            const content = desc.text();
+            const descContent = desc[0].innerText;
             // Дівка, де знаходиться наша кнопочка
-            const parent = desc.parents()[0];
+            const parent = desc.context;
             // Фіча. шо  не плодити куча кнопочок
             desc.remove();
             //Створюю нове поле з інпутом
             $(parent).prepend($('<input type="text"/>')
                 // В яке кладу значення
-                    .val(content)
+                    .val(descContent)
                     //Тут навішую колбек на івент brur - віджаття кнопки
                     .on('blur', event => onDescriptionInputBlur(id, event.target))
                     // і даю для нього клас, що б можна було знайти потім той клас
@@ -46,15 +54,22 @@ try {
         },
     });
 
-
+    /**
+     *  Set value from text field to div and save it in DataBase
+     * @param id - id of view from DataBase
+     * @param el - element where mouse was clicked
+     */
     const onDescriptionInputBlur = (id, el) => {
         const value = el.value.trim();
         const parent = $(el).parents()[0];
-        $(parent).prepend($('<div id="description" />').text(value));
+        $(parent).append($('<div id="description" />').text(value));
         Meteor.call("updateService", {id, description: value});
         $(el).remove();
     };
 
+    /**
+     * This method find all services in dataBase
+     */
     Template.services.helpers({
         findAllServices() {
             return Services.find({})
